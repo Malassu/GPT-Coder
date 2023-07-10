6db14c0a15bee399efb29b7a8da4943ed323a285
Ilkka Malassu <32188170+Malassu@users.noreply.github.com>
2023-07-10 18:08:06 +0300

    Create initial project and main form (#1)
    * Add initial project and main form
    
    * gitignore newline

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GptForm from './GptForm';
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>CoderGPT</h1>
+      <GptForm />
+    </div>
+  );
+}
+
+export default App;
